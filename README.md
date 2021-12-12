d05d398f0d2ed3db5670d70bc8ca3115091b524f
Matthias Gernand <matthias.gernand@fluxera.com>
2021-12-12 23:52:33 +0100

    Added initial project structure.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fluxera.Repository
+A generic repository implementation.
+
---
+++ src/Fluxera.Repository/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Fluxera.Repository")]
+[assembly: AssemblyDescription("A generic repository implementation.")]
