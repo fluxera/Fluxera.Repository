a57bd0dd9fabe1e1370f92deb74c1e0448fbdece
Matthias Gernand <matthias.gernand@fluxera.com>
2021-12-19 22:30:38 +0100

    Re-factored the abstractions.

--- tests/Fluxera.Repository.UnitTests/Decorators/InterceptionRepositoryDecoratorTests.cs
+++ tests/Fluxera.Repository.UnitTests/Decorators/InterceptionRepositoryDecoratorTests.cs
@@ -1,25 +1,18 @@
 ﻿namespace Fluxera.Repository.UnitTests.Decorators
 {
-	using System;
-	using Fluxera.Repository.Decorators;
-	using Fluxera.Repository.Interception;
-	using Fluxera.Repository.UnitTests.Core.PersonAggregate;
-	using Microsoft.Extensions.DependencyInjection;
-	using NUnit.Framework;
+	//[TestFixture]
+	//public class InterceptionRepositoryDecoratorTests : DecoratorTestBase
+	//{
+	//	/// <inheritdoc />
+	//	protected override Type DecoratorType => typeof(InterceptionRepositoryDecorator<,>);
 
-	[TestFixture]
-	public class InterceptionRepositoryDecoratorTests : DecoratorTestBase
-	{
-		/// <inheritdoc />
-		protected override Type DecoratorType => typeof(InterceptionRepositoryDecorator<,>);
+	//	/// <inheritdoc />
+	//	protected override Type RepositoryType => typeof(NoopTestRepository<Person, Guid>);
 
-		/// <inheritdoc />
-		protected override Type RepositoryType => typeof(NoopTestRepository<Person, Guid>);
-
-		/// <inheritdoc />
-		protected override void ConfigureServices(IServiceCollection services)
-		{
-			services.AddSingleton(typeof(IDecoratingInterceptorFactory<,>), typeof(TestDecoratingInterceptorFactory<,>));
-		}
-	}
+	//	/// <inheritdoc />
+	//	protected override void ConfigureServices(IServiceCollection services)
+	//	{
+	//		services.AddSingleton(typeof(IDecoratingInterceptorFactory<,>), typeof(TestDecoratingInterceptorFactory<,>));
+	//	}
+	//}
 }
