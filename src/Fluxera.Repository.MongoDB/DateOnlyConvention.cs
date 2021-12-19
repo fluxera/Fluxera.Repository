fa3a8be3ce9bb7dce207b738eb205b9bed94b545
Matthias Gernand <mgernand@users.noreply.github.com>
2021-12-19 22:31:04 +0100

    Re-factored the abstractions. (#7)

---
+++ src/Fluxera.Repository.MongoDB/DateOnlyConvention.cs
@@ -0,0 +1,66 @@
+﻿namespace Fluxera.Repository.MongoDB
+{
+	//internal sealed class TimeOnlyConvention : ConventionBase, IMemberMapConvention
+	//{
+	//	/// <inheritdoc />
+	//	public void Apply(BsonMemberMap memberMap)
+	//	{
+	//		Type originalMemberType = memberMap.MemberType;
+	//		Type memberType = originalMemberType.UnwrapNullableType();
+
+	//		if(memberType == typeof(TimeOnly))
+	//		{
+	//			DateTimeSerializer dateTimeSerializer = DateTimeSerializer.UtcInstance
+	//				.WithRepresentation(BsonType.String)
+	//				.WithDateOnly(true);
+
+	//			memberMap.SetSerializer(dateTimeSerializer);
+
+	//			if(originalMemberType.IsNullable())
+	//			{
+	//				memberMap.SetSerializer(dateTimeSerializer);
+	//			}
+	//			else
+	//			{
+	//				memberMap.SetSerializer(new NullableSerializer<DateTime>(dateTimeSerializer));
+	//			}
+	//		}
+	//	}
+	//}
+
+	//internal sealed class DateOnlyConvention : ConventionBase, IMemberMapConvention
+	//{
+	//	/// <inheritdoc />
+	//	public void Apply(BsonMemberMap memberMap)
+	//	{
+	//		Type originalMemberType = memberMap.MemberType;
+	//		Type memberType = originalMemberType.UnwrapNullableType();
+
+	//		if(memberType == typeof(DateOnly))
+	//		{
+	//			DateOnlySerializer dateTimeSerializer = DateOnlySerializer.Instance
+	//				.WithRepresentation(BsonType.String)
+	//				.WithDateOnly(true);
+
+	//			memberMap.SetSerializer(dateTimeSerializer);
+
+	//			if(originalMemberType.IsNullable())
+	//			{
+	//				memberMap.SetSerializer(dateTimeSerializer);
+	//			}
+	//			else
+	//			{
+	//				memberMap.SetSerializer(new NullableSerializer<DateOnly>(dateTimeSerializer));
+	//			}
+	//		}
+	//	}
+	//}
+
+	//internal sealed class DateOnlySerializer : StructSerializerBase<DateOnly>
+	//{
+	//	private static readonly DateOnlySerializer __localInstance = new DateOnlySerializer();
+
+
+	//	public static DateOnlySerializer Instance => instance;
+	//}
+}
