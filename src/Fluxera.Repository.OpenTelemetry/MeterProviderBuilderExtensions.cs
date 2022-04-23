43b563344f1e38b868135fdab2acd82c3a423efe
Matthias Gernand <matthias.gernand@fluxera.com>
2022-04-23 20:49:13 +0200

    Added extension methods for enabling the metering for the repository.

---
+++ src/Fluxera.Repository.OpenTelemetry/MeterProviderBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace OpenTelemetry.Trace
+{
+	using JetBrains.Annotations;
+	using OpenTelemetry.Metrics;
+
+	/// <summary>
+	///     Provides extension methods to enable the meter instrumentation for the Repository.
+	/// </summary>
+	[PublicAPI]
+	public static class MeterProviderBuilderExtensions
+	{
+		/// <summary>
+		///     Adds the meter for the repository to OpenTelemetry.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		public static MeterProviderBuilder AddRepositoryInstrumentation(this MeterProviderBuilder builder)
+		{
+			return builder.AddMeter("Fluxera.Repository");
+		}
+	}
+}
