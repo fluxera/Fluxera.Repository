071cf225ed70fe1728d4494685c8dee3d346f760
Matthias Gernand <matthias.gernand@fluxera.com>
2024-03-20 19:02:18 +0100

    Updated packages.

---
+++ tests/Fluxera.Repository.LiteDB.IntegrationTests/GlobalFixture.cs
@@ -0,0 +1,28 @@
+﻿namespace Fluxera.Repository.LiteDB.IntegrationTests
+{
+	using System.IO;
+	using System.Threading.Tasks;
+	using NUnit.Framework;
+
+	[SetUpFixture]
+	public class GlobalFixture
+	{
+		[OneTimeSetUp]
+		public async Task OneTimeSetUp()
+		{
+			foreach(string file in Directory.EnumerateFiles(".", "*.db"))
+			{
+				File.Delete(file);
+			}
+		}
+
+		[OneTimeTearDown]
+		public async Task OneTimeTearDown()
+		{
+			foreach(string file in Directory.EnumerateFiles(".", "*.db"))
+			{
+				File.Delete(file);
+			}
+		}
+	}
+}
