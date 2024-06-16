7e786c6f25ca76af6c5fa34e792a434aa2d70572
Matthias Gernand <mgernand@users.noreply.github.com>
2024-06-17 00:53:56 +0200

    Added integration for the Queries library. (#134)

---
+++ src/Fluxera.Repository.Queries/QueryOptionsExtensions.cs
@@ -0,0 +1,133 @@
+﻿namespace Fluxera.Repository.Queries
+{
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using Fluxera.Queries.Expressions;
+	using Fluxera.Queries.Options;
+	using Fluxera.Repository.Query;
+	using Fluxera.Repository.Query.Impl;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///		Extension methods for the <see cref="QueryOptions"/> type.
+	/// </summary>
+	[PublicAPI]
+	public static class QueryOptionsExtensions
+	{
+		/// <summary>
+		///		Builds <see cref="IQueryOptions{T}"/> from the <see cref="QueryOptions"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="queryOptions"></param>
+		/// <returns></returns>
+		public static IQueryOptions<T> ToQueryOptions<T>(this QueryOptions queryOptions)
+			where T : class
+		{
+			IQueryOptionsBuilder<T> queryOptionsBuilder = QueryOptionsBuilder.CreateFor<T>();
+			return queryOptionsBuilder.Apply(queryOptions).Build();
+		}
+
+		/// <summary>
+		///		Applies the given <see cref="QueryOptions"/> to the <see cref="IQueryOptionsBuilder{T}"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="builder"></param>
+		/// <param name="queryOptions"></param>
+		/// <returns></returns>
+		public static IQueryOptionsBuilder<T> Apply<T>(this IQueryOptionsBuilder<T> builder, QueryOptions queryOptions)
+			where T : class
+		{
+			// 1. Apply the orderby options. 
+			builder.Apply(queryOptions.OrderBy);
+
+			// 2. Apply paging.
+			builder.Apply(queryOptions.Skip, queryOptions.Top);
+
+			return builder;
+		}
+
+		/// <summary>
+		///		Applies the given <see cref="OrderByQueryOption"/> to the <see cref="IQueryOptionsBuilder{T}"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="builder"></param>
+		/// <param name="orderByQueryOption"></param>
+		/// <returns></returns>
+		public static IQueryOptionsBuilder<T> Apply<T>(this IQueryOptionsBuilder<T> builder, OrderByQueryOption orderByQueryOption)
+			where T : class
+		{
+			if(orderByQueryOption is not null && orderByQueryOption.Properties.Count > 0)
+			{
+				IReadOnlyCollection<OrderByExpression<T>> orderByExpressions = orderByQueryOption.ToOrderByExpressions<T>();
+
+				builder.Apply(orderByExpressions);
+			}
+
+			return builder;
+		}
+
+		/// <summary>
+		///		Applies the given <see cref="OrderByQueryOption"/> to the <see cref="IQueryOptionsBuilder{T}"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="builder"></param>
+		/// <param name="orderByExpressions"></param>
+		/// <returns></returns>
+		public static IQueryOptionsBuilder<T> Apply<T>(this IQueryOptionsBuilder<T> builder, IReadOnlyCollection<OrderByExpression<T>> orderByExpressions)
+			where T : class
+		{
+			bool isFirstClause = true;
+
+			ISortingOptions<T> sortingOptions = null;
+
+			foreach(OrderByExpression<T> expression in orderByExpressions)
+			{
+				if(isFirstClause)
+				{
+					sortingOptions = expression.Direction switch
+					{
+						OrderByDirection.Ascending  => builder.OrderBy(expression.SelectorExpression),
+						OrderByDirection.Descending => builder.OrderByDescending(expression.SelectorExpression),
+						_                           => throw new InvalidEnumArgumentException($"Unsupported order by direction {expression.Direction}.")
+					};
+				}
+				else
+				{
+					sortingOptions = expression.Direction switch
+					{
+						OrderByDirection.Ascending  => sortingOptions.ThenBy(expression.SelectorExpression),
+						OrderByDirection.Descending => sortingOptions.ThenByDescending(expression.SelectorExpression),
+						_                           => throw new InvalidEnumArgumentException($"Unsupported order by direction {expression.Direction}.")
+					};
+				}
+
+				isFirstClause = false;
+			}
+
+			return builder;
+		}
+
+		/// <summary>
+		///		Applies the given <see cref="OrderByQueryOption"/> to the <see cref="IQueryOptionsBuilder{T}"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="builder"></param>
+		/// <param name="skipQueryOption"></param>
+		/// <param name="topQueryOption"></param>
+		/// <returns></returns>
+		public static IQueryOptionsBuilder<T> Apply<T>(this IQueryOptionsBuilder<T> builder, SkipQueryOption skipQueryOption, TopQueryOption topQueryOption)
+			where T : class
+		{
+			ISkipTakeOptions<T> skipTakeOptions = skipQueryOption is not null
+				? builder.Skip(skipQueryOption.SkipValue ?? 0)
+				: builder.Skip(0);
+
+			if(topQueryOption?.TopValue != null)
+			{
+				skipTakeOptions.Take(topQueryOption.TopValue.Value);
+			}
+
+			return builder;
+		}
+	}
+}
---
+++ src/Fluxera.Repository.Queries/RepositoryExtensions.cs
@@ -0,0 +1,161 @@
+﻿namespace Fluxera.Repository.Queries
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq.Expressions;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using Fluxera.Entity;
+	using Fluxera.Guards;
+	using Fluxera.Queries;
+	using Fluxera.Queries.Options;
+	using Fluxera.Repository;
+	using Fluxera.Repository.Query;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///		Extension methods for use with the <see cref="IReadOnlyRepository{T, TKey}"/>.
+	/// </summary>
+	[PublicAPI]
+	public static class RepositoryExtensions
+	{
+		/// <summary>
+		///		Executes the get query defined by the given <typeparamref name="TKey"/> and <see cref="QueryOptions"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TKey"></typeparam>
+		/// <param name="repository"></param>
+		/// <param name="id"></param>
+		/// <param name="queryOptions"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public static async Task<SingleResult> ExecuteGetAsync<T, TKey>(this IReadOnlyRepository<T, TKey> repository,
+			TKey id,
+			QueryOptions queryOptions,
+			CancellationToken cancellationToken = default)
+			where T : AggregateRoot<T, TKey>
+			where TKey : IComparable<TKey>, IEquatable<TKey>
+		{
+			Guard.Against.Null(id);
+			Guard.Against.Null(repository);
+			Guard.Against.Null(queryOptions);
+
+			// 1. Build the selector expression (optional).
+			Expression<Func<T, T>> selector = queryOptions.ToSelector<T>();
+
+			// 2. Execute the get query.
+			T item = selector is null
+				? await repository.GetAsync(id, cancellationToken)
+				: await repository.GetAsync(id, selector, cancellationToken);
+
+			return new SingleResult(item);
+		}
+
+		/// <summary>
+		///		Executes the count query defined by the given <see cref="QueryOptions"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TKey"></typeparam>
+		/// <param name="repository"></param>
+		/// <param name="queryOptions"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public static async Task<long> ExecuteCountAsync<T, TKey>(this IReadOnlyRepository<T, TKey> repository,
+			QueryOptions queryOptions,
+			CancellationToken cancellationToken = default)
+			where T : AggregateRoot<T, TKey>
+			where TKey : IComparable<TKey>, IEquatable<TKey>
+		{
+			Guard.Against.Null(repository);
+			Guard.Against.Null(queryOptions);
+
+			// 1. Build the query predicate.
+			Expression<Func<T, bool>> predicate = queryOptions.ToPredicate<T>();
+
+			// 2. Execute the count query.
+			long count = await repository.CountAsync(predicate, cancellationToken);
+
+			return count;
+		}
+
+		/// <summary>
+		///		Executes the find many query defined by the given <see cref="QueryOptions"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TKey"></typeparam>
+		/// <param name="repository"></param>
+		/// <param name="queryOptions"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public static async Task<QueryResult> ExecuteFindManyAsync<T, TKey>(this IReadOnlyRepository<T, TKey> repository,
+			QueryOptions queryOptions,
+			CancellationToken cancellationToken = default)
+			where T : AggregateRoot<T, TKey>
+			where TKey : IComparable<TKey>, IEquatable<TKey>
+		{
+			Guard.Against.Null(repository);
+			Guard.Against.Null(queryOptions);
+
+			// 1. Build the query options: sorting and paging.
+			IQueryOptions<T> options = queryOptions.ToQueryOptions<T>();
+
+			// 2. Build the query predicate.
+			Expression<Func<T, bool>> predicate = queryOptions.ToPredicate<T>();
+
+			// 3. Build the selector expression (optional).
+			Expression<Func<T, T>> selector = queryOptions.ToSelector<T>();
+
+			// 4. Get the total count of the query (optional).
+			long? totalCount = null;
+			if(queryOptions.Count is not null)
+			{
+				if(queryOptions.Count.CountValue)
+				{
+					totalCount = await repository.CountAsync(predicate, cancellationToken);
+				}
+			}
+
+			// 5. Execute the find many query.
+			IReadOnlyCollection<T> items = selector is null
+				? await repository.FindManyAsync(predicate, options, cancellationToken)
+				: await repository.FindManyAsync(predicate, selector, options, cancellationToken);
+
+			return new QueryResult(items, totalCount);
+		}
+
+		/// <summary>
+		///		Executes the find one query defined by the given <see cref="QueryOptions"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TKey"></typeparam>
+		/// <param name="repository"></param>
+		/// <param name="queryOptions"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public static async Task<T> ExecuteFindOneAsync<T, TKey>(this IReadOnlyRepository<T, TKey> repository,
+			QueryOptions queryOptions,
+			CancellationToken cancellationToken = default)
+			where T : AggregateRoot<T, TKey>
+			where TKey : IComparable<TKey>, IEquatable<TKey>
+		{
+			Guard.Against.Null(repository);
+			Guard.Against.Null(queryOptions);
+
+			// 1. Build the query options: sorting and paging.
+			IQueryOptions<T> options = queryOptions.ToQueryOptions<T>();
+
+			// 2. Build the query predicate.
+			Expression<Func<T, bool>> predicate = queryOptions.ToPredicate<T>();
+
+			// 3. Build the selector expression (optional).
+			Expression<Func<T, T>> selector = queryOptions.ToSelector<T>();
+
+			// 4. Execute the find many query.
+			T item = selector is null
+				? await repository.FindOneAsync(predicate, options, cancellationToken)
+				: await repository.FindOneAsync(predicate, selector, options, cancellationToken);
+
+			return item;
+		}
+	}
+}
